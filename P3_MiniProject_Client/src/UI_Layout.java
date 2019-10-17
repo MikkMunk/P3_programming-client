2312721a7fecda6256365b91ea3d896eccdc127f
Sara886a <43111320+Sara886a@users.noreply.github.com>
2019-10-17 13:37:11 +0200

    Base UI layout set up

---
+++ P3_MiniProject_Client/src/UI_Layout.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class UI_Layout {
+    private JPanel panel1;
+    private JTextField text_card1;
+    private JTextField text_card2;
+    private JTextField text_card3;
+    private JTextField text_card4;
+    private JTextField text_card5;
+    private JTextField text_card6;
+    private JTextField text_card7;
+    private JTextField text_card8;
+    private JTextField text_card9;
+    private JTextField text_card10;
+    private JTextField text_card11;
+    private JTextField text_card12;
+    private JTextField text_card13;
+    private JTextField text_card14;
+    private JTextField text_card15;
+    private JTextField text_card16;
+    private JTextField text_card17;
+    private JTextField text_card18;
+    private JTextField text_card19;
+    private JTextField text_card20;
+    private JTextField text_card21;
+    private JTextField text_card22;
+    private JTextField text_card23;
+    private JTextField text_card24;
+    private JTextField text_card25;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
